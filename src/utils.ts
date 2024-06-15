13654b66b21bf0a83f566aa104baccda2e6cadbd
stephenlit <stevish45@icloud.com>
2024-06-15 13:24:13 -0300

    Change date formatter in utils to type date

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 //format the date to a string
-function formatDate(date: string): string {
+function formatDate(date: Date): string {
     const options: Intl.DateTimeFormatOptions = {
         year: 'numeric',
         month: 'long',

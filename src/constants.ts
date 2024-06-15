cb287d24cbf9fb56f8d4b9c21ceebaf22d830ef2
stephenlit <stevish45@icloud.com>
2024-06-15 13:01:41 -0300

    Initial Commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Tech People Blog';
+export const SITE_DESCRIPTION = 'Articles, Stories & Tutorials for Tech People';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+//format the date to a string
+function formatDate(date: string): string {
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+    return new Date(date).toLocaleDateString(undefined, options);
+}
+
+//Capitalze the first letter
+function capitalize(str: string): string {
+    if (typeof str !== 'string' || str.length === 0) {
+        return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export { formatDate, capitalize };
